8cf1dbd1840b9d15e3a2b4b1fb5f6d3a64e11e6d
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-20 16:17:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,5 +55,5 @@ def get_average_age_for_season(data, season)
     number_contestants += 1
   end
     average_age = average_age.to_i/number_contestants.to_f
-    return average_age
+    return average_age.to_i
 end
