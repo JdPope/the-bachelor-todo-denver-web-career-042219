35e94258adc3b07fe73fdae78431e55a95f49c28
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-20 15:56:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,39 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |name, name_data|
+    if name["status"] == "Winner"
+    
+      name = name["name"].split(" ")
+      return name[0]
+    end
+  end
 end
+
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_data|
+    season_data.each do |contestant_data|
+        if contestant_data["occupation"] == occupation
+          return contestant_data["name"]
+      end
+    end
+  end
 end
 
+
+
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_counter = 0 
+  data.each do |season, season_data|
+    season_data.each do |contestant_data|
+        if contestant_data["hometown"] == hometown
+          hometown_counter = hometown_counter + 1
+        end
+      end
+    end
+    return hometown_counter
 end
 
 def get_occupation(data, hometown)
